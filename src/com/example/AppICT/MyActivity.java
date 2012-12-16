beedf3366352fcf51ec81373a46dbc174eb30b38
Kyra Hameleers <kyrahameleers@hotmail.com>
2012-12-16 22:00:50 +0100

    trail en error met buttons

--- src/com/example/AppICT/MyActivity.java
+++ src/com/example/AppICT/MyActivity.java
@@ -1,6 +1,7 @@
 package com.example.AppICT;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,23 +11,37 @@ public class MyActivity extends Activity {
      * Called when the activity is first created.
      */
 
-    private Button button;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        button =(Button)findViewById(R.id.button1);
+        //button =(Button)findViewById(R.id.button1);
+        //button.setOnClickListener(new View.OnClickListener() {
+          //  public void onClick(View v) {
 
-        button.setOnClickListener(buttonClickListener);
-        //button.setOnLongClickListener(buttonLongClickListener);
+           // setContentView(R.layout.nid);
+
+                Button button1 = (Button)findViewById(R.id.btn_iti);
+                Button button2 = (Button)findViewById(R.id.btn_contact);
+                Button button3 = (Button)findViewById(R.id.btn_dp);
+                Button button4 = (Button)findViewById(R.id.btn_fg);
+                Button button5 = (Button)findViewById(R.id.btn_im);
+                Button button6 = (Button)findViewById(R.id.btn_nid);
+                Button button7 = (Button)findViewById(R.id.btn_nvg);
+                Button button8 = (Button)findViewById(R.id.btn_sw);
+            }
+        //});
+    public void iti()
+    {
+        Intent i = new Intent(this, iti.class) ;
+        startActivity(i);
     }
 
-    View.OnClickListener buttonClickListener = new View.OnClickListener(){
-        public void onClick(View v)
-        {
-              setContentView(R.layout.nid);
-        }
+    //View.OnClickListener buttonClickListener = new View.OnClickListener(){
+      //  public void onClick(View v)
+        //{
+          //    setContentView(R.layout.nid);
+        //}
     };
-}
+
---
+++ src/com/example/AppICT/iti.java
@@ -0,0 +1,35 @@
+package com.example.AppICT;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kyra
+ * Date: 16-12-12
+ * Time: 19:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class iti {
+
+
+
+    private Button button;
+
+    private void OpenInfo()
+    {
+    //button =(Button)findViewById(R.id.button1);
+    //button.setOnClickListener(new View.OnClickListener() {
+      //  public void onClick(View v) {
+
+        //}
+    //});
+        //LayoutInflater factory = LayoutInflater.from(R.layout.iti);
+        //final View textBoxView = factory.inflate(R.layout.iti, null);
+    }
+}
