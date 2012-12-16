341aefeff01ad5e461fafdd660f84581c45fbe7c
Kyra Hameleers <kyrahameleers@hotmail.com>
2012-12-16 18:43:15 +0100

    Extra schermen

--- src/com/example/AppICT/MyActivity.java
+++ src/com/example/AppICT/MyActivity.java
@@ -2,14 +2,31 @@ package com.example.AppICT;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private Button button;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        button =(Button)findViewById(R.id.button1);
+
+        button.setOnClickListener(buttonClickListener);
+        //button.setOnLongClickListener(buttonLongClickListener);
     }
+
+    View.OnClickListener buttonClickListener = new View.OnClickListener(){
+        public void onClick(View v)
+        {
+              setContentView(R.layout.nid);
+        }
+    };
 }
