fda8f0b27914a9298ff6ba87592f0efe22fd87ad
Kyra Hameleers <kyrahameleers@hotmail.com>
2012-12-20 14:14:44 +0100

    Nieuwe knopjes

---
+++ src/com/example/AppICT/Algemeen_Info.java
@@ -0,0 +1,18 @@
+package com.example.AppICT;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kyra
+ * Date: 19-12-12
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Algemeen_Info extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alle_studies);
+    }
+}
--- src/com/example/AppICT/MyActivity.java
+++ src/com/example/AppICT/MyActivity.java
@@ -18,12 +18,19 @@ public class MyActivity extends Activity {
 
         Button btn_iti = (Button)findViewById(R.id.btn_iti);
         Button btn_contact = (Button)findViewById(R.id.btn_contact);
+        Button btn_AlleStudies = (Button)findViewById(R.id.btn_AlleStudies);
         Button btn_dp = (Button)findViewById(R.id.btn_dp);
         Button btn_fg = (Button)findViewById(R.id.btn_fg);
         Button btn_im = (Button)findViewById(R.id.btn_im);
         Button btn_nid = (Button)findViewById(R.id.btn_nid);
         Button btn_nvg = (Button)findViewById(R.id.btn_nvg);
         Button btn_sw = (Button)findViewById(R.id.btn_sw);
+    }
+
+    public void alleStudies(View v)
+    {
+        Intent i = new Intent(this, Algemeen_Info.class);
+        startActivity(i);
     }
 
     public void iti(View v){
