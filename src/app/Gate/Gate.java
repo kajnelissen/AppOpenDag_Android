5d7c39447ad984638deb54cda76721d0fac6ebe0
Kaj Nelissen <kaj@kj91.nl>
2013-01-23 14:38:23 +0100

    Contract IGate
    Methodes gedefinieerd voor IGate interface.

--- src/app/Gate/Gate.java
+++ src/app/Gate/Gate.java
@@ -1,7 +1,17 @@
 package app.Gate;
 
 /**
+ * Gate
  *
+ * Standaardimplementatie van IGate die 
+ * gebruik maakt van HTTP-requests om te
+ * communiceren met de back-end API.
  */
-public class Gate {
+public class Gate implements IGate {
+	
+	public Gate()
+	{
+		
+	}
+
 }
